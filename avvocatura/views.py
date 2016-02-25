e3724c00befeccb105a1a41e9f1d7a9bf0aa998c
stonaz <stefano.tonazzi@gmail.com>
2016-02-25 19:30:52 +0100

    Muliple subdeps management

--- avvocatura/views.py
+++ avvocatura/views.py
@@ -11,43 +11,52 @@ def index(request):
     context = {'host_list': host_list}
     return render(request, 'avvocatura/index.html', context)
 
+def drawDeps(dep,node,graph):
+    if type(dep) is unicode:
+        graph.add_edge(node,dep)
+        print dep
+    if type(dep) is dict:
+        print 'subdep found:' 
+        DrawSubDeps(dep,node,graph)
+        
+def DrawSubDeps(dep,node,graph):
+    for dep,subdeps in dep.iteritems():
+        graph.add_edge(node,dep)
+        for subdep in subdeps:
+            drawDeps(subdep,dep,graph)
+            print type(subdep)
+            
+        #graph.add_edge(dep,subdep)
+
 def detail(request,id):
     host_list = Server.objects.all()
-    #client = MongoClient()
-    #db = client.test
-    #collection = db.avvocatura
     server = Server.objects.get(id=id)
     
-    print server
     node = server.name
     deps = server.deps
     desc = server.desc
     filename = 'avvocatura/static/avvocatura/' + node + '.png'
     context_filename = 'avvocatura/' + node + '.png'
-    print desc
-    print settings.STATIC_ROOT
+    #print settings.STATIC_ROOT
     if settings.DEBUG == False :
         filename = settings.STATIC_ROOT + 'avvocatura/'+ node + '.png'
-    print filename
-    
-
-    #for server in collection.find():
-    #    print server
+    #print filename
 
     G=pgv.AGraph(strict=False,directed=True)
     G.add_node(node,shape='box',style='filled',color=".7 .3 1.0") # adds node 'a'
     for dep in deps:
-        print type(dep)
-        if type(dep) is dict:
-            print 'subdep found'
-            for dep,subdeps in dep.iteritems():
-                for subdep in subdeps:
-                    print type(subdep)
-                    G.add_edge(dep,subdep)
-        #print dep[1]
-        G.add_edge(node,dep)
+        drawDeps(dep,node,G)
+        #G.add_edge(node,dep)
+        #print type(dep)
+        #if type(dep) is dict:
+        #    print 'subdep found'
+        #    for dep,subdeps in dep.iteritems():
+        #        for subdep in subdeps:
+        #            print type(subdep)
+        #            G.add_edge(dep,subdep)
+        #G.add_edge(node,dep)
     G.layout(prog='dot')    
     G.draw(filename)
-    G.write("postgres.dot")
+    #G.write("postgres.dot")
     context = {'host_list': host_list,'filename':context_filename, 'server' : server}
     return render(request, 'avvocatura/showpng.html', context)
